14ef07f99fef104142390bc35fe0dd1ce9beba89
Dharshini865 <100557486+Dharshini865@users.noreply.github.com>
2022-03-31 16:20:10 +0530

    InternshipTasks

--- minute-to-sec-convertor.js
+++ minute-to-sec-convertor.js
@@ -8,5 +8,5 @@ function minToSec(minutes) {
     return Math.floor(minutes * 60); 
 }
 
-let seconds = minToSec(5); 
+let seconds = minToSec(4); 
 console.log("Result of conversion:" + seconds + " seconds");
