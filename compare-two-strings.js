edd96a8e2690da7a34560e80ae8bfeda35036962
Dharshini865 <100557486+Dharshini865@users.noreply.github.com>
2022-04-13 11:51:15 +0530

    Update compare-two-strings.js

--- compare-two-strings.js
+++ compare-two-strings.js
@@ -1,10 +1,3 @@
-/** 
- * compare two strings
- * @param {string} str1 - str1 must be a string or string literal
- * @param {string} str2 - str2 must be a string or string literal
- * @returns {boolean} - length of the strings is equal or not
-*/
-
 function compareStrings(str1, str2) {
    
     if (str1.length == str2.length) {
