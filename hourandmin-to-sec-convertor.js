92239fd0e8b501c79ff3539fc4434af2eef0aea0
Dharshini865 <100557486+Dharshini865@users.noreply.github.com>
2022-04-13 12:01:57 +0530

    Update hourandmin-to-sec-convertor.js

--- hourandmin-to-sec-convertor.js
+++ hourandmin-to-sec-convertor.js
@@ -1,9 +1,4 @@
-/**
- * convert hours and minutes to seconds
- * @param {number} hours - hours must be a natural number
- * @param {number} minutes - minutes must be a natural number
- * @returns {number} - returns seconds of given hours and minutes
- */ 
+
 
 function hrMinToSec(hours, minutes) {
     return Math.floor( (hours * 60 * 60) + (minutes * 60) ); 
